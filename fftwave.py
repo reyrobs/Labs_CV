356563fdb1d42d7970bc022c527880d55a348ff1
AGarciaCast <a.gcastellanos@alumnos.upm.es>
2022-11-08 20:21:18 +0100

    up to multiplication

--- fftwave.py
+++ fftwave.py
@@ -5,7 +5,7 @@ from Functions import showgrey
 def fftwave(u, v, sz = 128):
 	Fhat = np.zeros([sz, sz])
 	Fhat[u, v] = 1
-	
+
 	F = np.fft.ifft2(Fhat)
 	Fabsmax = np.max(np.abs(F))
 
@@ -14,8 +14,11 @@ def fftwave(u, v, sz = 128):
 	plt.rc('axes', titlesize=10)
 	a1 = f.add_subplot(3, 2, 1)
 	showgrey(Fhat, False)
+	plt.axhline(y=0, color="r", linestyle="-")
+	plt.axvline(x=0, color="r", linestyle="-")
+
 	a1.title.set_text("Fhat: (u, v) = (%d, %d)" % (u, v))
-	
+
 	# What is done by these instructions?
 	if u < sz/2:
 		uc = u
@@ -26,28 +29,31 @@ def fftwave(u, v, sz = 128):
 	else:
 		vc = v - sz
 
-	wavelength = 0.0 # Replace by correct expression
-	amplitude  = 0.0 # Replace by correct expression
-	
+	wavelength = 1./np.sqrt(uc**2 + vc**2)
+	amplitude  = 1./(sz)**2
+
 	a2 = f.add_subplot(3, 2, 2)
 	showgrey(np.fft.fftshift(Fhat), False)
+	plt.axhline(y=sz/2, color="r", linestyle="-")
+	plt.axvline(x=sz/2, color="r", linestyle="-")
 	a2.title.set_text("centered Fhat: (uc, vc) = (%d, %d)" % (uc, vc))
-	
+
 	a3 = f.add_subplot(3, 2, 3)
+	print(np.max(np.real(F)), 1/(128)**2)
 	showgrey(np.real(F), False, 64, -Fabsmax, Fabsmax)
 	a3.title.set_text("real(F)")
-	
+
 	a4 = f.add_subplot(3, 2, 4)
 	showgrey(np.imag(F), False, 64, -Fabsmax, Fabsmax)
 	a4.title.set_text("imag(F)")
-	
+
 	a5 = f.add_subplot(3, 2, 5)
 	showgrey(np.abs(F), False, 64, -Fabsmax, Fabsmax)
 	a5.title.set_text("abs(F) (amplitude %f)" % amplitude)
-	
+
 	a6 = f.add_subplot(3, 2, 6)
 	showgrey(np.angle(F), False, 64, -np.pi, np.pi)
 	a6.title.set_text("angle(F) (wavelength %f)" % wavelength)
-	
+
 	plt.show()
 
--- lab1.py
+++ lab1.py
@@ -14,15 +14,76 @@ from fftwave import fftwave
 # depending on what you are working on. It should be fairly easy for a TA
 # to go through all parts of your code though.
 
-if __name__ == '__main__':
-    p,q = 5, 0
-    Fhat = np.zeros((128, 128))
-    Fhat[p,q] = 1
-    # showgrey(Fhat)
 
-    F = ifft2(Fhat)
-    Fabsmax = np.max(np.abs(F))
-    showgrey(np.real(F), True, 64, -Fabsmax, Fabsmax)
-    showgrey(np.imag(F), True, 64, -Fabsmax, Fabsmax)
-    showgrey(np.abs(F), True, 64, -Fabsmax, Fabsmax)
-    showgrey(np.angle(F), True, 64, -np.pi, np.pi)
+def ex1():
+    fftwave(5, 9)
+    fftwave(9, 5)
+    fftwave(17, 9)
+    fftwave(17, 121)
+    fftwave(5, 1)
+    fftwave(125, 1)
+
+def ex2(use_log=True):
+    F = np.concatenate([np.zeros((56,128)), np.ones((16,128)), np.zeros((56,128))])
+    G = F.T
+    H = F + 2*G
+    fig = plt.figure()
+    _ = fig.add_subplot(3, 3, 1)
+    showgrey(F, False)
+    _ = fig.add_subplot(3, 3, 2)
+    showgrey(G, False)
+    _ = fig.add_subplot(3, 3, 3)
+    showgrey(H, False)
+    Fhat = fft2(F)
+    Ghat = fft2(G)
+    Hhat = fft2(H)
+    
+    _ = fig.add_subplot(3, 3, 4)
+    showgrey(np.log(1 + np.abs(Fhat)), False)
+    
+    _ = fig.add_subplot(3, 3, 5)
+    showgrey(np.log(1 + np.abs(Ghat)), False)
+    
+    _ = fig.add_subplot(3, 3, 6)
+    showgrey(np.log(1 + np.abs(Hhat)), False)
+    
+    _ = fig.add_subplot(3, 3, 7)
+    shift_Fhat = fftshift(Fhat)
+    if use_log:
+        showgrey(np.log(1 + np.abs(shift_Fhat)), False)
+    else:
+        showgrey(np.abs(shift_Fhat), False)
+        
+    _ = fig.add_subplot(3, 3, 8)
+    shift_Ghat = fftshift(Ghat)
+    if use_log:
+        showgrey(np.log(1 + np.abs(shift_Ghat)), False)
+    else:
+        showgrey(np.abs(shift_Ghat), False)
+                
+    _ = fig.add_subplot(3, 3, 9)
+    shift_Hhat = fftshift(Hhat)
+    if use_log:
+        showgrey(np.log(1 + np.abs(shift_Hhat)), False)
+        # showfs(Hhat, False)
+    else:
+        showgrey(np.abs(shift_Hhat), False)
+    
+    plt.show()
+    
+    
+if __name__ == '__main__':
+
+    exercise = 2
+
+    if exercise == 1:
+        ex1()
+    elif exercise == 2:
+        ex2(use_log=True)
+    else:
+        pass
+    
+ 
+
+
+    
